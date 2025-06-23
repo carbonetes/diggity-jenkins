5d6dddc41dafeb7cc013cd369cea8dec280feeb4
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2025-06-23 14:35:24 +0800

    chore: always use latest binary version diggity on diggity-jenkins plugin

---
+++ src/main/java/io/jenkins/plugins/diggity/install/CheckVersion.java
@@ -0,0 +1,40 @@
+package io.jenkins.plugins.diggity.install;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class CheckVersion {
+
+    public String getVersion() {
+        String repositoryUrl = "https://github.com/carbonetes/diggity";
+        try {
+            String latestVersion = getLatestVersion(repositoryUrl);
+            return latestVersion;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String getLatestVersion(String repositoryUrl) throws IOException {
+        String apiUrl = repositoryUrl.replace("github.com", "api.github.com/repos") + "/releases/latest";
+        URL url = new URL(apiUrl);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/vnd.github.v3+json");
+
+        try (InputStream inputStream = connection.getInputStream()) {
+            Scanner scanner = new Scanner(inputStream, "UTF-8");
+            String response = scanner.useDelimiter("\\A").next();
+            scanner.close();
+
+            // Parse the JSON response to extract the latest version
+            // Assuming the response is in the format: {"tag_name": "vX.X.X"}
+            return response.split("\"tag_name\":")[1].split("\"")[1];
+        }
+    }
+}
--- src/main/java/io/jenkins/plugins/diggity/install/DiggityExist.java
+++ src/main/java/io/jenkins/plugins/diggity/install/DiggityExist.java
@@ -10,14 +10,16 @@ import hudson.FilePath;
 public class DiggityExist {
 
     public Boolean checkIfExists(FilePath workspace) {
+        CheckVersion checkVersion = new CheckVersion();
         String workspacePath = workspace.getRemote();
-        String version = "v1.17.1";
-        String filename = "diggity-" + version + "-exist.txt";
+        String version = checkVersion.getVersion();
+        String fileName = "diggity-" + version + "-Exist.txt";
         String fileContent = "Diggity" + version + " installed on this workspace";
-        Boolean fileExists = checkFileExists(workspacePath, filename);
+
+        Boolean fileExists = checkFileExists(workspacePath, fileName);
         if (Boolean.FALSE.equals(fileExists)) {
             try {
-                String filePath = workspacePath + File.separator + filename;
+                String filePath = workspacePath + File.separator + fileName;
                 try (FileWriter writer = new FileWriter(filePath, StandardCharsets.UTF_8)) {
                     writer.write(fileContent);
                 }
@@ -28,12 +30,10 @@ public class DiggityExist {
         } else {
             return true;
         }
-
     }
 
-    private Boolean checkFileExists(String workspacePath, String filename) {
-        File file = new File(workspacePath, filename);
+    private Boolean checkFileExists(String workspacePath, String fileName) {
+        File file = new File(workspacePath, fileName);
         return file.exists() && !file.isDirectory();
     }
-    
 }
